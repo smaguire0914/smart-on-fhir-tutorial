7211e91230bf2d493e62669734c713692a7bfe51
smaguire0914 <samantha.maguire@dbhds.virginia.gov>
2026-04-29 13:17:58 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,9 +15,12 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: ['http://loinc.org|8302-2',//body height 
+                              'http://loinc.org|8462-4',//diastolic blood pressure
+                              'http://loinc.org|8480-6',//systolic blood pressure
+                              'http://loinc.org|2085-9',//HDL
+                              'http://loinc.org|2089-1',//LDL
+                              'http://loinc.org|85354-9']//BP systolic diastolic
                       }
                     }
                   });
